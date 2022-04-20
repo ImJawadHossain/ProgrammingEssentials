234b38f049dc52e8424611ebf874879fbe88de1e
Md Jawad Hossain <jawadsgs3@gmail.com>
2022-04-20 18:08:12 +0600

    Create GetTriangleArea.py

---
+++ GetTriangleArea.py
@@ -0,0 +1,12 @@
+import math
+a = int(input("Enter the value of a: "))
+b = int(input("Enter the value of b: "))
+c = int(input("Enter the value of c: "))
+S = (a+b+c)/2
+
+if (a+b) > c and (a+c) > b and (b+c) > a:
+    area = math.sqrt(S*(S-a)*(S-b)*(S-c))
+    print(area)
+
+else:
+    print("Triangle is not Possible.")
